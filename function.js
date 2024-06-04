d332fc3ef210385fc20eafee967269599144c8c2
sam <samnygard@gmail.com>
2024-06-04 11:11:02 -0700

    keyboard inputs are working

--- function.js
+++ function.js
@@ -2,6 +2,13 @@ const display = document.querySelector(".display");
 
 display.textContent = "";
 
+document.addEventListener("keydown", function(e) {
+    console.log(e.key)
+    numberKeys(e);
+    operationKeys(e);
+    backspaceKey(e);
+    enterKey(e);
+});
 const numbers = document.querySelectorAll(".number");
 
 numbers.forEach((number) =>{
@@ -26,7 +33,6 @@ operations.forEach((operation) =>{
         else{
             display.textContent = display.textContent.slice(0, -3);
             display.textContent = display.textContent.concat(operation.textContent);
-
         }
 
         if(pointTracker){
@@ -72,6 +78,144 @@ equal.addEventListener("click", ()=>{
 });
 
 function Calculate(){
-    numbers = display.textContent.split(" * ");
-    console.log(numbers);
+    const re = /\s.\s/g;
+    let numbers = display.textContent.split(re);
+    const re2 = /\W(?=\s)/g;
+    let operators = display.textContent.match(re2);
+
+    // console.log(numbers);
+    // console.log(operators);
+
+    
+
+    // for(let i = 0; numbers.length; i++){
+    //     numbers[i] = Number(numbers[i]);
+    // }
+    multiply(numbers, operators);
+    divide(numbers, operators);
+    minus(numbers, operators);
+    plus(numbers, operators);
+    display.textContent = Number(numbers[0]).toFixed(2).toString();
+    // console.log(numbers, operators);
+}
+
+function multiply(numbers, operators){
+    let flag = true;
+    while(flag){
+        let index = operators.indexOf("*");
+        // console.log(index);
+        if(index != -1){
+            operators.splice(index, 1);
+            numbers[index] = (Number(numbers[index]) * Number(numbers[index +1])).toString();
+            // console.log(numbers);
+            // console.log(operators);
+            numbers.splice((index+1), 1);
+        }
+        else{
+            flag = false;
+        }
+    }
+}
+
+function divide(numbers, operators){
+    let flag = true;
+    while(flag){
+        let index = operators.indexOf("/");
+        // console.log(index);
+        if(index != -1){
+            operators.splice(index, 1);
+            numbers[index] = (Number(numbers[index]) / Number(numbers[index +1])).toString();
+            // console.log(numbers);
+            // console.log(operators);
+            numbers.splice((index+1), 1);
+        }
+        else{
+            flag = false;
+        }
+    }
+}
+
+function minus(numbers, operators){
+    let flag = true;
+    while(flag){
+        let index = operators.indexOf("-");
+        // console.log(index);
+        if(index != -1){
+            operators.splice(index, 1);
+            numbers[index] = (Number(numbers[index]) - Number(numbers[index +1])).toString();
+            // console.log(numbers);
+            // console.log(operators);
+            numbers.splice((index+1), 1);
+        }
+        else{
+            flag = false;
+        }
+    }
+}
+
+function plus(numbers, operators){
+    let flag = true;
+    while(flag){
+        let index = operators.indexOf("+");
+        // console.log(index);
+        if(index != -1){
+            operators.splice(index, 1);
+            numbers[index] = (Number(numbers[index]) + Number(numbers[index +1])).toString();
+            // console.log(numbers);
+            // console.log(operators);
+            numbers.splice((index+1), 1);
+        }
+        else{
+            flag = false;
+        }
+    }
+}
+
+function numberKeys(e){
+    if(Number(e.key) >= 0 && Number(e.key) <= 9){
+        display.textContent = display.textContent.concat(e.key);
+        if(operationTracker){
+            operationTracker = false;
+        }
+    }
+}
+
+function operationKeys(e){
+    if(e.key == "/" || e.key == "*" || e.key == "+" || e.key == "-"){
+        let toAdd = " " + e.key + " ";
+        console.log(toAdd);
+        if(operationTracker == false){
+            display.textContent = display.textContent.concat(toAdd);
+            operationTracker = true;
+        }
+        else{
+            display.textContent = display.textContent.slice(0, -3);
+            display.textContent = display.textContent.concat(toAdd);
+        }
+
+        if(pointTracker){
+            pointTracker = false;
+        }
+    }
+}
+
+function backspaceKey(e){
+    if(e.code == "Backspace"){
+        if(operationTracker){
+            display.textContent = display.textContent.slice(0, -3);
+            operationTracker = false;
+        }
+        else{
+            display.textContent = display.textContent.slice(0, -1);
+            if(display.textContent.slice(-1) == " "){
+                operationTracker = true;
+            }
+        }
+    }
+}
+
+function enterKey(e){
+    if(e.key == "Enter"){
+        Calculate();
+    }
 }
